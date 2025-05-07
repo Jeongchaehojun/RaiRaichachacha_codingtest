04ba33979ef7e3710990c99478c2edb8cc27996d
Jeong chaehojun <hohoho07197@gmail.com>
2025-05-07 14:36:11 +0900

    Add files via upload

---
+++ Bronze5/1809_moo.cpp
@@ -0,0 +1,17 @@
+//백준 1809번 moo
+//소 그림을 그리는거임ㅋㅋ
+//2025.05.07
+//브론즈5문제
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "   (___)" << endl;
+    cout << "   (o o)____/" << endl;
+    cout << "    @@      \\" << endl;
+    cout << "     \\ ____,/" << endl;
+    cout << "     //   //" << endl;
+    cout << "    ^^   ^^" << endl;
+    return 0;
+}
+//근데 백준 제출은 Golfscript만 되게 해놓았네..
