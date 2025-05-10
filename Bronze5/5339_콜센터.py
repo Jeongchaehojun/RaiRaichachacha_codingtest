e826d0225dfcd729c94b478fcb4cdb582e6c2cc5
Jeong chaehojun <hohoho07197@gmail.com>
2025-05-10 14:54:51 +0900

    Add files via upload

---
+++ Bronze5/5339_콜센터.py
@@ -0,0 +1,12 @@
+#2025.05.10
+#bronze 5
+print("     /~\\")
+print("    ( oo|")
+print("    _\\=/_")
+print("   /  _  \\")
+print("  //|/.\|\\\\")
+print(" ||  \\ /  ||")
+print("============")
+print("|          |")
+print("|          |")
+print("|          |")
