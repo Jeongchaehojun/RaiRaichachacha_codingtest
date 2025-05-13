9d04649147edc88e6e179448960931b64320520f
Jeong chaehojun <hohoho07197@gmail.com>
2025-05-13 11:59:45 +0900

    Add files via upload

---
+++ Bronze5/21300_bottlereturn.py
@@ -0,0 +1,4 @@
+#2025.05.13
+#b5
+B, M, W, C, S, H = map(int, input().split())
+print(B*5+M*5+W*5+C*5+S*5+H*5)
---
+++ Bronze5/21598_싸이컴.py
@@ -0,0 +1,8 @@
+#2025.05.13
+#b5
+N = int(input())
+if(N==1):
+    print("SciComLove")
+else:
+    print("SciComLove")
+    print("SciComLove")
