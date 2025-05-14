812919282e07f9ccab97ee782c0f0328532cba1d
Jeong chaehojun <hohoho07197@gmail.com>
2025-05-14 14:03:09 +0900

    Add files via upload

---
+++ Bronze5/24883_자동완성.py
@@ -0,0 +1,7 @@
+#2025.05.14
+#b5
+alphabet = input()
+if(alphabet == 'N' or alphabet == 'n'):
+    print("Naver D2")
+else:
+    print("Naver Whale")
---
+++ Bronze5/25314_코딩은체육.py
@@ -0,0 +1,6 @@
+#2025.05.14
+#b5
+N = int(input())
+longnum = N//4
+result = "long "*longnum + "int"
+print(result)
