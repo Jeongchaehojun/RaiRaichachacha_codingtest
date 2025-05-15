445756740c01ac0dbec77b007de65709084377e5
Jeong chaehojun <hohoho07197@gmail.com>
2025-05-15 14:56:13 +0900

    Add files via upload

---
+++ Bronze5/25372_성택이.py
@@ -0,0 +1,11 @@
+#2025.05.15
+#b5
+N = int(input())
+for _ in range(N):
+    length = len(input().strip())
+    if(6<=length<=9):
+        print("yes")
+    else: 
+        print("no")
+        #한 방에 맞춤 ㅋ
+        
