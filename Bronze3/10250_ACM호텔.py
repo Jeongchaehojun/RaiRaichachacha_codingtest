e790e673551d3fa36afe17dd20bfd902c458ffb1
Jeong chaehojun <hohoho07197@gmail.com>
2025-05-17 23:56:50 +0900

    Add files via upload

---
+++ Bronze3/10250_ACM호텔.py
@@ -0,0 +1,11 @@
+#2025.05.17
+#b3
+T = int(input())
+for _ in range(T):
+    H, W, N = map(int, input().split())
+    floor = N%H
+    room = N//H + 1
+    if floor == 0:
+        floor = H
+        room -= 1
+    print(f"{floor}{room:02}")
