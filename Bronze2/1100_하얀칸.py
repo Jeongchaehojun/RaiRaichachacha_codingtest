3d65e5910b97e95bfdead660ab86b4894c18e94c
Jeong chaehojun <hohoho07197@gmail.com>
2025-07-03 15:10:25 +0900

    Add files via upload

---
+++ Bronze2/1100_하얀칸.py
@@ -0,0 +1,9 @@
+#2025.07.03
+#b2
+count = 0
+for i in range(8):
+    row = input().strip()
+    for j in range(8):
+        if (i + j) % 2 == 0 and row[j] == 'F':
+            count += 1
+print(count)
