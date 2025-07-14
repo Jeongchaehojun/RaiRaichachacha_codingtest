be025194160e3d07518c4c72a2d44788555723d2
Jeong chaehojun <hohoho07197@gmail.com>
2025-07-14 16:58:28 +0900

    Add files via upload

--- Silver4/1205_등수구하기.py
+++ Silver4/1205_등수구하기.py
@@ -1,18 +1,21 @@
 #2025.07.14
 #s4
 N, S, P = map(int, input().split())
-
 if N > 0:
     scores = list(map(int, input().split()))
 else:
     scores = []
 
-scores.append(S)
-scores.sort(reverse=True)
+rank = 1
+for i in range(N):
+    if scores[i] > S:
+        rank += 1
+    else:
+        break
 
-rank = scores.index(S) + 1
-
-if N == P and scores[P-1] > S:
+# 랭킹이 꽉 찼고, 내 점수가 꼴찌보다 작거나 같으면 못 들어감
+if rank > P or (N==P and N>0 and S <= scores[N-1]):
     print(-1)
 else:
     print(rank)
+
