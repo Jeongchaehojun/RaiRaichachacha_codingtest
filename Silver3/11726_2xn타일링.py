37f2feaf5540a678687369767da64d2006b38600
Jeong chaehojun <hohoho07197@gmail.com>
2025-09-17 07:49:28 +0900

    Add files via upload

---
+++ Silver3/11726_2xn타일링.py
@@ -0,0 +1,16 @@
+#2025.09.17
+#s3
+#dp
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+dp = [0] * (n+1)
+
+dp[1] = 1
+if n >= 2:
+    dp[2] = 2
+
+for i in range(3, n+1):
+    dp[i] = (dp[i-1] + dp[i-2]) % 10007 #점화식
+print(dp[n])
