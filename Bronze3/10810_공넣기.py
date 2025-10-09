d8d4e46746c0315904b16acb67577c3e1a3d67e2
Jeong chaehojun <hohoho07197@gmail.com>
2025-10-09 21:29:37 +0900

    10810_공넣기.py 만들기

---
+++ Bronze3/10810_공넣기.py
@@ -0,0 +1,11 @@
+#2025.10.09
+# b3
+N, M = map(int, input().split())
+basket = [0] * N
+
+for _ in range(M):
+    i, j, k = map(int, input().split())
+    for x in range(i - 1, j): 
+        basket[x] = k
+
+print(*basket)
